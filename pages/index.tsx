91f96203fab252fe1979051b5a3e749124f03eb2
Thomas <thomas@enhance.com>
2024-07-29 01:16:49 +0100

    added titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-
 import { NavBar } from "@/components/NavBar";
+import { Fragment } from "react";
+import Head from "next/head";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col bg-gray-900 gap-2 ${inter.className}`}
-    >
-      <NavBar />
-      <div className="container mx-auto flex gap-5 max-w-xl px-4 text-gray-300">
-        <p>click the Feed link above to see posts ↑</p>
-      </div>
-    </main>
+    <Fragment>
+      <Head>
+        <title>Robots</title>
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col bg-gray-900 gap-2 ${inter.className}`}
+      >
+        <NavBar />
+        <div className="container mx-auto flex gap-5 max-w-xl px-4 text-gray-300">
+          <p>click the Feed link above to see posts ↑</p>
+        </div>
+      </main>
+    </Fragment>
   );
 }
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
-import Image from "next/image";
+import { Fragment } from "react";
 import { Inter } from "next/font/google";
 import { NavBar } from "../components/NavBar";
 import { PostList } from "@/components/Posts/PostList";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Feed() {
   return (
-    <main
-      className={`flex min-h-screen flex-col bg-gray-900 gap-2 ${inter.className}`}
-    >
-      <NavBar />
-      <div className="container mx-auto flex  items-center gap-5 max-w-xl">
-        <PostList />
-      </div>
-    </main>
+    <Fragment>
+      <Head>
+        <title>Robots | Feed</title>
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col bg-gray-900 gap-2 ${inter.className}`}
+      >
+        <NavBar />
+        <div className="container mx-auto flex  items-center gap-5 max-w-xl">
+          <PostList />
+        </div>
+      </main>
+    </Fragment>
   );
 }
